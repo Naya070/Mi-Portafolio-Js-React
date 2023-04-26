aff7c001bbe1a875d81bbf634ed5ea9570585e3b
Naya070 <nayarith070@gmail.com>
2023-04-26 14:07:49 -0400

    Se cambió el link defectuoso de linkedin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -39,7 +39,7 @@ function Footer() {
             </li> */}
             <li className="social-icons">
               <a
-                href="linkedin.com/in/nayarith-jiménez"
+                href="https://www.linkedin.com/in/nayarith-jiménez"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
